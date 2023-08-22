5b07632b34400c4ac9ee84378dbefc3548f6971b
Aleksandr Kravets <skrav@ukr.net>
2023-08-22 19:14:31 +0200

    add changes schema

--- controllers/register.js
+++ controllers/register.js
@@ -1,43 +1,41 @@
-const createError = require("http-errors");
-const { User } = require("../models");
-const { v4: uuidv4 } = require("uuid");
-const sgMail = require("@sendgrid/mail");
-const bcrypt = require("bcryptjs");
+const createError = require('http-errors');
+const { User } = require('../models');
+const { v4: uuidv4 } = require('uuid');
+const sgMail = require('@sendgrid/mail');
+const bcrypt = require('bcryptjs');
 
-const createLetter = require("../templates/letter");
+const createLetter = require('../templates/letter');
 
 const { SENDGRID_API_KEY } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const register = async (req, res, next) => {
+  const { email, password, name } = req.body;
 
-	const { email, password, name } = req.body;
+  try {
+    const existUser = await User.findOne({ email });
 
-	try {
-		const existUser = await User.findOne({ email });
+    if (existUser) {
+      throw createError(409);
+    }
 
-		if (existUser) {
-			throw createError(409);
-		}
+    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-		const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    const verificationToken = uuidv4();
+    await sgMail.send(createLetter(email, verificationToken));
 
-		const verificationToken = uuidv4();
-		await sgMail.send(createLetter(email, verificationToken));
-
-		const newUser = await User.create({
-			name,
-			email,
-			password: hashPassword,
-			avatarURL: null,
-			verificationToken,
-		});
-		res.status(201).json({ message: 'New account created' });
-	} catch (error) {
-		next(error);
-	}
-
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+      avatarURL: null,
+      verificationToken,
+    });
+    res.status(201).json({ message: 'New account created' });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = register;
