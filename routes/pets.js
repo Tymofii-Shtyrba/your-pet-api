fe9ad80f5351af2ba88dc6f7ad15a8fcffbea04c
Aleksandr Kravets <skrav@ukr.net>
2023-08-30 23:43:59 +0200

    add changes

--- routes/pets.js
+++ routes/pets.js
@@ -18,5 +18,6 @@ router.post('/', isValidToken, upload.single('image'), isValidPetsBody, addPet);
 router.get('/', isValidToken, getUserOwnedPets);
 
 router.delete('delete/:petId', isValidToken, isValidId, deletePetById);
+router.delete('/:petId', isValidToken, isValidId, deletePetById);
 
 module.exports = router;
