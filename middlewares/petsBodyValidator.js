95c3dc5ae1339b1ef6ff48f7d021884ff4f2520f
Aleksandr Kravets <skrav@ukr.net>
2023-09-02 13:17:29 +0200

    add swagger

--- middlewares/petsBodyValidator.js
+++ middlewares/petsBodyValidator.js
@@ -2,15 +2,10 @@ const Joi = require('joi');
 const createError = require('http-errors');
 
 const schema = new Joi.object({
-  category: Joi.string().required(),
   name: Joi.string().required(),
   date: Joi.string().required(),
   type: Joi.string().required(),
-  sex: Joi.string().required(),
-  location: Joi.string(),
-  price: Joi.number().allow(null),
-  comment: Joi.string(),
-  imageURL: Joi.any(),
+  comments: Joi.string().required(),
 });
 
 const isValidPetsBody = (req, res, next) => {
--- models/pet.js
+++ models/pet.js
@@ -1,108 +1,7 @@
 const { Schema, model } = require('mongoose');
 
-// const { handleMongooseError } = require('../helpers');
-
-// const petSchema = new Schema(
-//   {
-//     category: {
-//       type: String,
-//       enum: ['my-pet', 'sell', 'lost/found', 'in-good-hands'],
-//       required: [true, 'Please choose a valid option for chooseOption'],
-//     },
-
-//     name: {
-//       type: String,
-//       required: [true, 'The name of the pet is required'],
-//       minLength: 2,
-//       maxLength: 16,
-//     },
-
-//     date: {
-//       type: Date,
-//       required: true,
-//     },
-
-//     type: {
-//       type: String,
-//       required: [true, 'The type of the pet is required'],
-//       minLength: 2,
-//       maxLength: 16,
-//     },
-
-//     imageURL: {
-//       type: String,
-//       default: null,
-//     },
-
-//     publicId: {
-//       type: String,
-//       default: null,
-//     },
-
-//     sex: {
-//       type: String,
-//       enum: ['female', 'male'],
-//       required: [true, 'The sex of the pet is required'],
-//     },
-
-//     location: {
-//       type: String,
-//       required: function () {
-//         const value = this.category;
-
-//         if (value === 'my-pet') {
-//           return false;
-//         }
-//         return true;
-//       },
-//     },
-
-//     price: {
-//       type: Number,
-//       required: function () {
-//         return this.category === 'sell';
-//       },
-//       validate: [
-//         {
-//           validator: function (value) {
-//             return !(this.category === 'sell' && (isNaN(value) || value <= 0));
-//           },
-//           message:
-//             'For the "sell" category, price must be a number greater than 0.',
-//         },
-//       ],
-//     },
-
-//     comments: {
-//       type: String,
-//       minLength: 10,
-//       maxLength: 120,
-//     },
-
-//     owner: {
-//       type: Schema.Types.ObjectId,
-//       ref: 'user',
-//       required: true,
-//     },
-//   },
-
-//   { versionKey: false, timestamps: true }
-// );
-
-// petSchema.post('save', handleMongooseError);
-
-// const Pet = model('pet', petSchema);
-
-// module.exports = Pet;
-
 const schema = new Schema(
   {
-    category: {
-      type: String,
-      enum: ['my-pet', 'sell', 'lost/found', 'in-good-hands'],
-      required: [true, 'Please choose a valid option for chooseOption'],
-    },
-
     name: {
       type: String,
       required: [true, 'The name of the pet is required'],
@@ -129,22 +28,6 @@ const schema = new Schema(
 
     publicId: {
       type: String,
-      default: null,
-    },
-
-    sex: {
-      type: String,
-      enum: ['female', 'male'],
-      required: [false, 'The sex of the pet is not required'],
-    },
-
-    location: {
-      type: String,
-      default: null,
-    },
-
-    price: {
-      type: Number,
       default: null,
     },
 
