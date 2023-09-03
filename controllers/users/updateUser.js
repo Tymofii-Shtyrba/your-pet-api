89c418aac9a1614f84d663d1deea2a44a9f78d9b
Taras-Veremchuk <taras.veremm@gmail.com>
2023-09-03 19:16:48 +0200

    try +++

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -1,26 +1,11 @@
 const { User } = require('../../models');
-// const createError = require('http-errors');
-// const bcrypt = require('bcryptjs');
 
 const updateUser = async (req, res, next) => {
-  // const { _id } = req.user;
   const { body, userId } = req;
-  
-  console.log("🚀 req:", body)
-
   try {
-    // if (password) {
-    //   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    //   otherFields.password = hashPassword;
-    // }
-
     const result = await User.findByIdAndUpdate(userId, body, {
       new: true,
     });
-
-    // if (!result) {
-    //   throw createError(404);
-    // }
 console.log("result:", result)
     res.json(result);
   } catch (error) {
